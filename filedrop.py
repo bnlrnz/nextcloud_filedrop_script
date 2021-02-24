64248c6e3047b92882606f70a8036e1f9dd11f49
bnlrnz <lorenzb@tu-freiberg.de>
2021-02-24 12:08:24 +0100

    added stuff

---
+++ filedrop.py
@@ -0,0 +1,72 @@
+#!/usr/local/bin/python3
+
+import os
+import io
+import subprocess
+import requests
+import argparse
+import xml.etree.ElementTree as ElementTree
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('username', type=str)
+parser.add_argument('password', type=str)
+parser.add_argument('webdav_user_uuid', type=str) # check <root>/remote.php/dav/files/<webdav_user_uuid> See "Settings" in webinterface
+parser.add_argument('--input', '-i', help='file with foldernames', type=str, required=True)
+parser.add_argument('--url', '-u', type=str, default="https://ificloud.xsitepool.tu-freiberg.de")
+parser.add_argument('--verbose', action='count', default=0)
+
+args = parser.parse_args()
+
+with open(args.input) as file:
+    foldernames = file.readlines()
+
+foldernames = [name.strip() for name in foldernames]
+
+for foldername in foldernames:
+    # create folder
+    command_create = "curl -s -X MKCOL -u " 
+    command_create += args.username + ":" 
+    command_create += args.password + " '" 
+    command_create += args.url 
+    command_create += "/remote.php/dav/files/" 
+    command_create += args.webdav_user_uuid + "/" 
+    command_create += foldername + "'"
+
+    if args.verbose:
+        print(command_create)
+
+    os.system(command_create)
+
+    # share folder
+    command_share = "curl -s -X POST -u "
+    command_share += args.username + ":"
+    command_share += args.password + " -d "
+    command_share += "'path=/" + foldername + "&shareType=3&publicUpload=true&permissions=4' '"
+    command_share += args.url + "/ocs/v2.php/apps/files_sharing/api/v1/shares' -H \"OCS-APIRequest: true\""
+
+    if args.verbose:
+        print(command_share)
+
+    resp = subprocess.check_output(command_share, shell=True).decode('utf8').strip('\n')
+
+    if args.verbose:
+        print(resp)
+
+    root = ElementTree.fromstring(resp)
+
+    for urls in root.iter('url'):
+        print(foldername + " - " + urls.text)
+        
+    for ids in root.iter('id'):
+        # udpate perission
+        command_perm = "curl -s -o /dev/null -X PUT -u "
+        command_perm += args.username + ":"
+        command_perm += args.password + " -d "
+        command_perm += "'permissions=4' '"
+        command_perm += args.url + "/ocs/v2.php/apps/files_sharing/api/v1/shares/" + ids.text + "' -H \"OCS-APIRequest: true\""
+        
+        if args.verbose:
+            print(command_perm)
+            
+        os.system(command_perm)
